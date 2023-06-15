8c8cb1943f466f7a3107dc166b5b7bb32c47d9e4
DamianM <damian.michrowski@gmail.com>
2023-06-15 12:29:08 +0200

    Initial commit

---
+++ src/hla13/producerConsumer/consumer/ConsumerAmbassador.java
@@ -0,0 +1,154 @@
+package hla13.producerConsumer.consumer;
+
+import hla.rti.*;
+import hla.rti.jlc.EncodingHelpers;
+import hla.rti.jlc.NullFederateAmbassador;
+import hla13.Example13Federate;
+import hla13.producerConsumer.storage.ExternalEvent;
+import org.portico.impl.hla13.types.DoubleTime;
+
+/**
+ * Created by Michal on 2016-04-27.
+ */
+public class ConsumerAmbassador extends NullFederateAmbassador {
+
+    protected double federateTime        = 0.0;
+    protected double federateLookahead   = 1.0;
+    protected double grantedTime         = 0.0;
+    protected boolean isRegulating       = false;
+    protected boolean isConstrained      = false;
+    protected boolean isAdvancing        = false;
+
+    protected boolean isAnnounced        = false;
+    protected boolean isReadyToRun       = false;
+
+    protected boolean running 			 = true;
+    protected int getProductHandle = 0;
+
+    private double convertTime( LogicalTime logicalTime )
+    {
+        // PORTICO SPECIFIC!!
+        return ((DoubleTime)logicalTime).getTime();
+    }
+
+    private void log( String message )
+    {
+        System.out.println( "FederateAmbassador: " + message );
+    }
+
+    public void synchronizationPointRegistrationFailed( String label )
+    {
+        log( "Failed to register sync point: " + label );
+    }
+
+    public void synchronizationPointRegistrationSucceeded( String label )
+    {
+        log( "Successfully registered sync point: " + label );
+    }
+
+    public void announceSynchronizationPoint( String label, byte[] tag )
+    {
+        log( "Synchronization point announced: " + label );
+        if( label.equals(Example13Federate.READY_TO_RUN) )
+            this.isAnnounced = true;
+    }
+
+    public void federationSynchronized( String label )
+    {
+        log( "Federation Synchronized: " + label );
+        if( label.equals(Example13Federate.READY_TO_RUN) )
+            this.isReadyToRun = true;
+    }
+
+    /**
+     * The RTI has informed us that time regulation is now enabled.
+     */
+    public void timeRegulationEnabled( LogicalTime theFederateTime )
+    {
+        this.federateTime = convertTime( theFederateTime );
+        this.isRegulating = true;
+    }
+
+    public void timeConstrainedEnabled( LogicalTime theFederateTime )
+    {
+        this.federateTime = convertTime( theFederateTime );
+        this.isConstrained = true;
+    }
+
+    public void timeAdvanceGrant( LogicalTime theTime )
+    {
+        this.federateTime = convertTime( theTime );
+        this.isAdvancing = false;
+    }
+
+    public void receiveInteraction( int interactionClass,
+                                    ReceivedInteraction theInteraction,
+                                    byte[] tag )
+    {
+        // just pass it on to the other method for printing purposes
+        // passing null as the time will let the other method know it
+        // it from us, not from the RTI
+        receiveInteraction(interactionClass, theInteraction, tag, null, null);
+    }
+
+    public void receiveInteraction( int interactionClass,
+                                    ReceivedInteraction theInteraction,
+                                    byte[] tag,
+                                    LogicalTime theTime,
+                                    EventRetractionHandle eventRetractionHandle )
+    {
+        StringBuilder builder = new StringBuilder( "Interaction Received:" );
+        if(interactionClass == getProductHandle) {
+            try {
+                int id = EncodingHelpers.decodeInt(theInteraction.getValue(0));
+                double time =  convertTime(theTime);
+
+                builder.append("Zajecie Stolika , time=" + time);
+                builder.append(" id=").append(id);
+                builder.append( "\n" );
+
+            } catch (ArrayIndexOutOfBounds ignored) {
+
+            }
+
+        }
+
+        log( builder.toString() );
+    }
+
+    public void reflectAttributeValues(int theObject,
+                                       ReflectedAttributes theAttributes, byte[] tag) {
+        reflectAttributeValues(theObject, theAttributes, tag, null, null);
+    }
+
+    public void reflectAttributeValues(int theObject,
+                                       ReflectedAttributes theAttributes, byte[] tag, LogicalTime theTime,
+                                       EventRetractionHandle retractionHandle) {
+        StringBuilder builder = new StringBuilder("Reflection for object:");
+
+        builder.append(" handle=").append(theObject);
+//		builder.append(", tag=" + EncodingHelpers.decodeString(tag));
+
+        // print the attribute information
+        builder.append(", attributeCount=").append(theAttributes.size());
+        builder.append("\n");
+        for (int i = 0; i < theAttributes.size(); i++) {
+            try {
+                // print the attibute handle
+                builder.append("\tattributeHandle=");
+                builder.append(theAttributes.getAttributeHandle(i));
+                // print the attribute value
+                builder.append(", attributeValue=");
+                builder.append(EncodingHelpers.decodeInt(theAttributes
+                        .getValue(i)));
+                builder.append(", time=");
+                builder.append(theTime);
+                builder.append("\n");
+            } catch (ArrayIndexOutOfBounds aioob) {
+                // won't happen
+            }
+        }
+
+        log(builder.toString());
+    }
+}
---
+++ src/hla13/producerConsumer/storage/StorageFederate.java
@@ -0,0 +1,227 @@
+package hla13.producerConsumer.storage;
+
+
+import hla.rti.*;
+import hla.rti.jlc.EncodingHelpers;
+import hla.rti.jlc.RtiFactoryFactory;
+import org.portico.impl.hla13.types.DoubleTime;
+import org.portico.impl.hla13.types.DoubleTimeInterval;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.util.Collections;
+import java.util.Random;
+
+public class StorageFederate {
+
+    public static final String READY_TO_RUN = "ReadyToRun";
+
+    private RTIambassador rtiamb;
+    private StorageAmbassador fedamb;
+    private final double timeStep           = 10.0;
+
+    private int storageHlaHandle;
+
+    private int i = 1;
+
+    public void runFederate() throws Exception {
+
+        rtiamb = RtiFactoryFactory.getRtiFactory().createRtiAmbassador();
+
+        try
+        {
+            File fom = new File( "producer-consumer.fed" );
+            rtiamb.createFederationExecution( "ExampleFederation",
+                    fom.toURI().toURL() );
+            log( "Created Federation" );
+        }
+        catch( FederationExecutionAlreadyExists exists )
+        {
+            log( "Didn't create federation, it already existed" );
+        }
+        catch( MalformedURLException urle )
+        {
+            log( "Exception processing fom: " + urle.getMessage() );
+            urle.printStackTrace();
+            return;
+        }
+
+        fedamb = new StorageAmbassador();
+        rtiamb.joinFederationExecution( "StorageFederate", "ExampleFederation", fedamb );
+        log( "Joined Federation as StorageFederate");
+
+        rtiamb.registerFederationSynchronizationPoint( READY_TO_RUN, null );
+
+        while( fedamb.isAnnounced == false )
+        {
+            rtiamb.tick();
+        }
+
+        waitForUser();
+
+        rtiamb.synchronizationPointAchieved( READY_TO_RUN );
+        log( "Achieved sync point: " +READY_TO_RUN+ ", waiting for federation..." );
+        while( fedamb.isReadyToRun == false )
+        {
+            rtiamb.tick();
+        }
+
+        enableTimePolicy();
+
+        publishAndSubscribe();
+
+        registerStorageObject();
+
+        while (fedamb.running) {
+            double timeToAdvance = fedamb.federateTime + timeStep;
+            advanceTime(timeToAdvance);
+            sendInteraction(timeToAdvance + fedamb.federateLookahead);
+            if(fedamb.queue.size() > 0)
+                for(Integer id : fedamb.queue){
+
+                }
+            if(fedamb.grantedTime == timeToAdvance) {
+                timeToAdvance += fedamb.federateLookahead;
+                log("Updating stock at time: " + timeToAdvance);
+                updateHLAObject(timeToAdvance);
+                fedamb.federateTime = timeToAdvance;
+            }
+
+            rtiamb.tick();
+        }
+
+    }
+
+
+
+    private void waitForUser()
+    {
+        log( " >>>>>>>>>> Press Enter to Continue <<<<<<<<<<" );
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in) );
+        try
+        {
+            reader.readLine();
+        }
+        catch( Exception e )
+        {
+            log( "Error while waiting for user input: " + e.getMessage() );
+            e.printStackTrace();
+        }
+    }
+
+    private void registerStorageObject() throws RTIexception {
+        int classHandle = rtiamb.getObjectClassHandle("ObjectRoot.Storage");
+        this.storageHlaHandle = rtiamb.registerObjectInstance(classHandle);
+    }
+
+    private void updateHLAObject(double time) throws RTIexception{
+        SuppliedAttributes attributes =
+                RtiFactoryFactory.getRtiFactory().createSuppliedAttributes();
+
+        int classHandle = rtiamb.getObjectClass(storageHlaHandle);
+        int stockHandle = rtiamb.getAttributeHandle( "stock", classHandle );
+        byte[] stockValue = EncodingHelpers.encodeInt(i);
+
+        attributes.add(stockHandle, stockValue);
+        LogicalTime logicalTime = convertTime( time );
+        rtiamb.updateAttributeValues( storageHlaHandle, attributes, "actualize stock".getBytes(), logicalTime );
+    }
+
+    private void sendInteraction(double timeStep) throws RTIexception {
+        SuppliedParameters parameters =
+                RtiFactoryFactory.getRtiFactory().createSuppliedParameters();
+        Random random = new Random();
+        byte[] id = EncodingHelpers.encodeInt(i++);
+
+        int interactionHandle = rtiamb.getInteractionClassHandle("InteractionRoot.ZajecieStolika");
+        int idHandle = rtiamb.getParameterHandle( "id", interactionHandle );
+
+        parameters.add(idHandle, id);
+
+        LogicalTime time = convertTime( timeStep );
+        rtiamb.sendInteraction( interactionHandle, parameters, "tag".getBytes(), time );
+    }
+
+    private void advanceTime( double timeToAdvance ) throws RTIexception {
+        fedamb.isAdvancing = true;
+        LogicalTime newTime = convertTime( timeToAdvance );
+        rtiamb.timeAdvanceRequest( newTime );
+
+        while( fedamb.isAdvancing )
+        {
+            rtiamb.tick();
+        }
+    }
+
+
+    private void publishAndSubscribe() throws RTIexception {
+
+        int classHandle = rtiamb.getObjectClassHandle("ObjectRoot.Storage");
+        int stockHandle    = rtiamb.getAttributeHandle( "stock", classHandle );
+
+        AttributeHandleSet attributes =
+                RtiFactoryFactory.getRtiFactory().createAttributeHandleSet();
+        attributes.add( stockHandle );
+        rtiamb.publishObjectClass(classHandle, attributes);
+        //rtiamb.subscribeObjectClassAttributes(classHandle, attributes);
+
+
+        int dojscieDoKolejkiHandle = rtiamb.getInteractionClassHandle( "InteractionRoot.DojscieDoKolejki" );
+        fedamb.dojscieDoKolejkiHandle = dojscieDoKolejkiHandle;
+        rtiamb.subscribeInteractionClass( dojscieDoKolejkiHandle );
+
+        int getProductHandle = rtiamb.getInteractionClassHandle( "InteractionRoot.ZajecieStolika" );
+        rtiamb.publishInteractionClass(getProductHandle);
+    }
+
+    private void enableTimePolicy() throws RTIexception
+    {
+        LogicalTime currentTime = convertTime( fedamb.federateTime );
+        LogicalTimeInterval lookahead = convertInterval( fedamb.federateLookahead );
+
+        this.rtiamb.enableTimeRegulation( currentTime, lookahead );
+
+        while( fedamb.isRegulating == false )
+        {
+            rtiamb.tick();
+        }
+
+        this.rtiamb.enableTimeConstrained();
+
+        while( fedamb.isConstrained == false )
+        {
+            rtiamb.tick();
+        }
+    }
+
+    private LogicalTime convertTime( double time )
+    {
+        // PORTICO SPECIFIC!!
+        return new DoubleTime( time );
+    }
+
+    /**
+     * Same as for {@link #convertTime(double)}
+     */
+    private LogicalTimeInterval convertInterval( double time )
+    {
+        // PORTICO SPECIFIC!!
+        return new DoubleTimeInterval( time );
+    }
+
+    private void log( String message )
+    {
+        System.out.println( "StorageFederate   : " + message );
+    }
+
+    public static void main(String[] args) {
+        try {
+            new StorageFederate().runFederate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
